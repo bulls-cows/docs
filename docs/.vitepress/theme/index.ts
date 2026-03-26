1fe84136b8db667a788b0f47ad00f79256cb3b6b
Yakima <yakima.public@gmail.com>
2026-03-26 22:46:33 +0800

    chore: 初始化项目开发环境配置并支持 SCSS 样式

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,7 +2,7 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import './style.css'
+import './styles/style.scss'
 
 export default {
   extends: DefaultTheme,
